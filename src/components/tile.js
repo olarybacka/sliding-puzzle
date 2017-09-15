ec414d8d45a1312a383f98e469a325eb24475ca7
olarybacka <rybacka.ola@gmail.com>
2017-09-15 16:08:01 +0200

    added images and component

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
-import './Tile.css';
 
 
 const StyledTile = styled.section`
 text-align: center;
-background: tomato;
 width: ${props => props.size}px;
 height: ${props => props.size}px;
 position: absolute;
 left:  ${props => props.left}px;
 top: ${props => props.top}px;
+background: ${props => (props.id == 0) ? 'none' : 'url("public/img/puzzle/1/' + props.id + '.jpg") tomato'};
 border: 1px solid #fff;
 `;
-
-export default (props) => {
-    console.log(props)
-    const {size, i, dimension} = props
-    return (
-        <StyledTile 
-            left = {size * (i % dimension)} 
-            top = { Math.floor(i / dimension) * size}
-            size={size} 
-            className="tile">  
-        </StyledTile>
-    )
+// if (i === 0) { item.className += " empty" }
+// else { item.style.backgroundImage = `url("img/puzzle/${this.folder}/${i}.jpg")` };
+class TileComponent extends Component {
+    render() {
+        const {size, i, dimension} = this.props
+        this.id = i;
+        return (
+            <StyledTile
+                left={size * (i % dimension)}
+                top={Math.floor(i / dimension) * size}
+                size={size}
+                id={i}
+                className="tile">
+            </StyledTile>
+        )
+    }
 }
+
+export default TileComponent;
+
