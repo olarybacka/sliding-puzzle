3186831a55d92a9a44361b78ae18a531c2c2b59e
Ola <rybacka.ola@gmail.com>
2017-09-17 08:58:19 +0200

    images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Board from './components/Board'
+import Board from './components/board'
 
 class App extends Component {
   render() {
--- src/components/tile.js
+++ src/components/tile.js
@@ -3,17 +3,18 @@ import styled from 'styled-components';
 
 
 const StyledTile = styled.section`
-text-align: center;
-width: ${props => props.size}px;
-height: ${props => props.size}px;
-position: absolute;
-left:  ${props => props.left}px;
-top: ${props => props.top}px;
-background: ${props => (props.id == 0) ? 'none' : 'url("public/img/puzzle/1/' + props.id + '.jpg") tomato'};
-border: 1px solid #fff;
+    text-align: center;
+    width: ${props => props.size}px;
+    height: ${props => props.size}px;
+    position: absolute;
+    left:  ${props => props.left}px;
+    top: ${props => props.top}px;
+    background: ${props => (props.id === 0) ? 'none' : 'url("./img/puzzle/1/' + props.id + '.jpg") tomato'};
+    background-size: cover;
+    border: 1px solid #fff;
 `;
-// if (i === 0) { item.className += " empty" }
-// else { item.style.backgroundImage = `url("img/puzzle/${this.folder}/${i}.jpg")` };
+
+
 class TileComponent extends Component {
     render() {
         const {size, i, dimension} = this.props
