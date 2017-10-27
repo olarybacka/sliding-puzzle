e925da250c5e308f9bdf30b6b83225ff406cf0a9
olarybacka <rybacka.ola@gmail.com>
2017-10-27 10:40:04 +0200

    handle collisions

--- src/containers/board.js
+++ src/containers/board.js
@@ -13,7 +13,6 @@ class BoardComponent extends Component {
                     window.innerWidth > 500 ? 130 :
                         100
         ),
-        margin: 0,
         dimension: 3, // n x n tiles
         empty: {
             top: 0,
@@ -28,39 +27,41 @@ class BoardComponent extends Component {
     saveInitialState = (state) => {
         const completedPoisition = this.state.completedPoisition;
         completedPoisition[state.id] = { ...state }
-        this.setState(prevState => ({
-            completedPoisition
-        }))
+        this.setState({ completedPoisition })
     }
 
-    saveCurrentState = async (state) => {
+    saveCurrentState = (state) => {
         setTimeout(() => {
-            const currentPosition = JSON.parse(JSON.stringify(this.state.currentPosition));
+            const currentPosition = JSON.parse(JSON.stringify(this.state.currentPosition)); // deep copy to avoid looping in shouldComponentUpdate
             currentPosition[state.id] = { ...state }
-            this.setState(prevState => ({
-                currentPosition
-            }))
+            this.setState({ currentPosition })
         })
     }
 
     shouldComponentUpdate(nextProps, nextState) {
         return (nextState.currentPosition === this.state.currentPosition ? true : false)
     }
-    isNextToEmpty(tileObj) {
-        return Math.abs(tileObj.row - this.emptyObj.row) + Math.abs(tileObj.column - this.emptyObj.column) === 1;
-    }
+
     moveTile = (id, left, top) => {
-        this.setState({
-            id: id,
-            active: {
-                top: this.state.empty.top,
-                left: this.state.empty.left
-            },
-            empty: {
-                top: top,
-                left: left
-            },
-        })
+        if (this.isNextToEmpty({ id, left, top })) {
+            this.setState({
+                id: id,
+                active: {
+                    top: this.state.empty.top,
+                    left: this.state.empty.left
+                },
+                empty: {
+                    top: top,
+                    left: left
+                },
+            })
+        }
+    }
+
+    isNextToEmpty(tile) {
+        const { size, empty } = this.state;
+        const distance = (Math.abs(tile.left - empty.left) + Math.abs(tile.top - empty.top))
+        return distance === size
     }
 
     render() {
